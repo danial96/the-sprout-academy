f69a72a76aa06e13e095549be97aa507764984cb
Daniyal Khan <huzaifaalam36@gmail.com>
2026-06-30 16:44:15 +0500

    Add temporary location seeder script

---
+++ public/seed_locations.php
@@ -0,0 +1,10 @@
+<?php
+define('LARAVEL_ROOT', dirname(__DIR__));
+require LARAVEL_ROOT . '/vendor/autoload.php';
+$app = require_once LARAVEL_ROOT . '/bootstrap/app.php';
+$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+$kernel->bootstrap();
+
+Artisan::call('db:seed', ['--class' => 'LocationSeeder', '--force' => true]);
+echo Artisan::output();
+echo "Done!";
