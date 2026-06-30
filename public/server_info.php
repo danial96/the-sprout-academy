d4ba985fc8be7e52204f97442b473b4e6a36bb56
Daniyal Khan <huzaifaalam36@gmail.com>
2026-06-30 18:11:45 +0500

    Remove temporary setup/debug scripts from codebase

--- public/server_info.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "Server IP: " . $_SERVER['SERVER_ADDR'] . "\n";
-echo "Host: " . gethostname() . "\n";
-$ip = file_get_contents('https://api.ipify.org');
-echo "Outbound IP: " . $ip . "\n";
