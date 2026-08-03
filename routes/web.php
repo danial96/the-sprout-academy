5f50c0a403fec76ecb0fec1aeadb4207cdb161e5
Daniyal Khan <huzaifaalam36@gmail.com>
2026-08-03 22:54:53 +0500

    Add temporary debug route for upload settings

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,20 @@ use App\Http\Controllers\EmployeeRegisterController;
 
 
 
+// TEMPORARY - Debug upload settings
+Route::get('/debug-upload-k9x2m', function () {
+    return response()->json([
+        'upload_max_filesize' => ini_get('upload_max_filesize'),
+        'post_max_size'       => ini_get('post_max_size'),
+        'file_uploads'        => ini_get('file_uploads'),
+        'max_file_uploads'    => ini_get('max_file_uploads'),
+        'upload_tmp_dir'      => ini_get('upload_tmp_dir'),
+        'tmp_dir_writable'    => is_writable(sys_get_temp_dir()),
+        'storage_writable'    => is_writable(storage_path('app/public')),
+        'php_version'         => PHP_VERSION,
+    ]);
+});
+
 // TEMPORARY - Clear SproutVine news cache
 Route::get('/clear-news-cache-k7m3x', function () {
     \Illuminate\Support\Facades\Cache::forget('sproutvine_news');
